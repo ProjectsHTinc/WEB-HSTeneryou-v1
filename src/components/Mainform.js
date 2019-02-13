564d8cc4901803980d1bc340a471239f6605a3f2
kamalhappysanz <kamal.happysanz@gmail.com>
2019-02-13 13:01:42 +0530

    Update Mainform.js

--- src/components/Mainform.js
+++ src/components/Mainform.js
@@ -10,7 +10,7 @@ export class Mainform extends Component{
     constructor(props) {
         super(props);
         this.state = {
-            step:5,
+            step:1,
             roof_inclination :'DEGREES_0',
             living_area:'',
             post_code:'',
